83fad224c93d2bdb07f126893289bc423e6b4cf7
hongcha <h0n9cha@gmail.com>
2015-08-13 22:33:22 +0800

    first

---
+++ php-function-unset.c
@@ -0,0 +1,33 @@
+/**
+ * 实现php 释放函数功能(function_unset)  类似变量 unset  
+ *   
+ */
+PHP_FUNCTION(function_unset)
+{
+    char *name;
+    int name_len;
+    zend_function *func;
+    char *lcname;
+    zend_bool retval;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &name, &name_len) == FAILURE) {
+        return;
+    }   
+
+    lcname = zend_str_tolower_dup(name, name_len);
+
+
+    /* Ignore leading "\" */
+    name = lcname;
+    if (lcname[0] == '\\') {
+        name = &lcname[1];
+        name_len--;
+    }   
+
+    retval = (zend_hash_del_key_or_index(EG(function_table), name, name_len+1, 0, 0) == SUCCESS);
+
+    efree(lcname);
+
+    RETURN_BOOL(retval);
+}
+
